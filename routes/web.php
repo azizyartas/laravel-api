e2cd54af163beaa6e8c9305ac426d21cc4ab2b20
azizyartas <67240879+azizyartas@users.noreply.github.com>
2024-09-30 19:29:02 +0300

    Add setup route to initialize admin user and generate tokens
    A new route `/setup` has been added to initialize an admin user with predefined credentials if not already present. This route also generates and returns admin, update, and basic API tokens for the newly created admin user.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,38 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/setup', function () {
+    $credentails = [
+        'email' => 'admin@gmail.com',
+        'password' => 'password',
+    ];
+
+    if (!Auth::attempt($credentails)) {
+        $user = new User();
+
+        $user->name = 'Admin';
+        $user->email = 'admin@gmail.com';
+        $user->password = Hash::make($credentails['password']);
+
+        $user->save();
+        if (Auth::attempt($credentails)) {
+            $user = Auth::user();
+
+            $adminToken = $user->createToken('admin-token', ['create', 'update', 'delete']);
+            $updateToken = $user->createToken('update-token', ['create', 'update']);
+            $basicToken = $user->createToken('bosic-token');
+
+            return [
+                'admin' => $adminToken->plainTextToken,
+                'update' => $updateToken->plainTextToken,
+                'basic' => $basicToken->plainTextToken,
+            ];
+        }
+    }
 });
