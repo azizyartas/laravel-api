b163517b9d1719a02b8895c044fc7c998b17c489
azizyartas <67240879+azizyartas@users.noreply.github.com>
2024-09-30 19:36:15 +0300

    Enhance authorization checks for request classes
    Update the 'authorize' methods in UpdateCustomerRequest, BulkStoreInvoiceRequest, and StoreCustomerRequest to require token permissions. This ensures that only authenticated users with the appropriate permissions can perform these actions.

--- app/Http/Requests/StoreCustomerRequest.php
+++ app/Http/Requests/StoreCustomerRequest.php
@@ -14,7 +14,8 @@ class StoreCustomerRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        $user = $this->user();
+        return $user != null && $user->tokenCan('create');
     }
 
     /**
